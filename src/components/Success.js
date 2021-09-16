a51ed66c4b392dc20d1e211d56218db2bbec8584
Justine MANGA <jmarymanga@gmail.com>
2021-09-16 23:09:39 +0000

    P12_React_JS

---
+++ src/components/Success.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Success extends Component {
+    render() {
+        return (
+            <div>
+                <h1 className="text-white">Compte Créer avec Succés</h1>
+            </div>
+        )
+    }
+}
+
+export default Success
